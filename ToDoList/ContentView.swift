7ef31736453b7faed63b450a2afddd0dc1554f28
Zo-hack <enzoarantes2000@gmail.com>
2025-03-10 01:19:23 -0400

    Initial Commit

---
+++ ToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Enzo Arantes on 3/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Enzo Arantes on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
